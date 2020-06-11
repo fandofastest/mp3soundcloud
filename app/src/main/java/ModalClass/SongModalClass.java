3dc2a4f05b9d24ad1904692af9c14f01b2befb9b
fando <fandofast@gmail.com>
2020-06-11 16:59:02 +0700

    Initial commit

---
+++ app/src/main/java/ModalClass/SongModalClass.java
@@ -0,0 +1,60 @@
+package ModalClass;
+
+/**
+ * Created by Remmss on 29-08-2017.
+ */
+
+public class SongModalClass {
+
+    int img;
+    String SongName;
+    String ArtistName;
+    String duration;
+    String imgurl;
+    String id;
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getImgurl() {
+        return imgurl;
+    }
+
+    public void setImgurl(String imgurl) {
+        this.imgurl = imgurl;
+    }
+
+
+
+
+    public String getSongName() {
+        return SongName;
+    }
+
+    public void setSongName(String songName) {
+        SongName = songName;
+    }
+
+    public String getArtistName() {
+        return ArtistName;
+    }
+
+    public void setArtistName(String artistName) {
+        ArtistName = artistName;
+    }
+
+
+}
